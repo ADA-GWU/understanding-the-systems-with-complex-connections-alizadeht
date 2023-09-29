34dc51045ae112cf46fb6b4e603580b862c4e95e
alizadeht <turalalizadeh4@gmail.com>
2023-09-29 17:42:43 +0400

    Update client.c

--- client.c
+++ client.c
@@ -19,3 +19,13 @@ int main() {
         perror("Socket creation failed"); // It gives an error message when problem occurs.
         exit(EXIT_FAILURE);
     }
+
+    // Configuration of server addresses and ports
+    struct sockaddr_in server_addrs[SERVER_COUNT];
+    int ports[SERVER_COUNT] = {12345, 12346, 12347}; // defining the port numbers
+
+    for (int i = 0; i < SERVER_COUNT; i++) {
+        server_addrs[i].sin_family = AF_INET;
+        server_addrs[i].sin_port = htons(ports[i]);
+        server_addrs[i].sin_addr.s_addr = inet_addr("127.0.0.1"); //server address
+    }

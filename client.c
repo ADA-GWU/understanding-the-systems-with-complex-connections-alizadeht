4ea3de3582a6faa495a3dfcc18cf26c1e0ffb222
alizadeht <turalalizadeh4@gmail.com>
2023-10-01 19:02:07 +0400

    Update client.c

--- client.c
+++ client.c
@@ -11,17 +11,17 @@
 #define MAX_PORT 49151  // Maximum port number that can be entered
 
 int main() {
-    int client_socket;
-    struct sockaddr_in server_addr;
+    char input[1024];
     char buffer[1024];
-    int num;
+    struct sockaddr_in server_addr;
+    long long num; 
 
-    // From this line, the process is about socket creation.
-    client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket == -1) {
-        perror("Socket creation failed"); // It gives an error message when problem occurs.
-        exit(EXIT_FAILURE);
-    }
+    while (1) {
+        int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+        if (client_socket == -1) {
+            perror("Socket creation failed"); // Error message
+            exit(EXIT_FAILURE);
+        }
 
     // Configuration of server addresses and ports
     struct sockaddr_in server_addrs[SERVER_COUNT];
@@ -32,10 +32,10 @@ int main() {
         server_addrs[i].sin_port = htons(ports[i]);
         server_addrs[i].sin_addr.s_addr = inet_addr("127.0.0.1"); //server address
     }
-    while (1) {
-        // It gives us connection to the random server (with rand() function)
-        int selected_server = rand() % SERVER_COUNT;
-        if (connect(client_socket, (struct sockaddr *)&server_addrs[selected_server], sizeof(server_addrs[selected_server])) == -1) {
-            perror("Connection to server failed"); // It gives an error message when server could not be detected.
-            exit(EXIT_FAILURE);
-        }
+    // Connect to a random server
+    int selected_server = rand() % SERVER_COUNT;
+    if (connect(client_socket, (struct sockaddr *)&server_addrs[selected_server], sizeof(server_addrs[selected_server])) == -1) {
+        perror("Failure of connection to server. Try again"); // Error message
+        close(client_socket);
+        continue; 
+    }
